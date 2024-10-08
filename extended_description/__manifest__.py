5a3f99ae1ffb1e0eca50d459d14b7ebbf1c5af57
Pablo Castelo <pablocv@gmx.es>
2024-10-09 00:18:22 +0200

    extended_description

---
+++ extended_description/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "extended_description",
+
+    'summary': """Allows to insert extended descriptions on sale orders""",
+
+    'description': """Allows to insert extended descriptions on sale orders""",
+
+    'author': "zvERP",
+    'website': "https://www.zverp.com",
+    'license': "AGPL-3",
+    'depends': ['sale'],
+    'category': 'Uncategorized',
+    'version': '15.0.1.0',
+    'data': [
+        'views/product.xml',
+    ],
+}
