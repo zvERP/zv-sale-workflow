27b5f84ac9413d7dbb5ac18d6ee15e0096e449f8
Pablo Castelo <pablocv@gmx.es>
2024-12-26 17:20:20 +0100

    remove is not overriding the create method in batch warning

--- extended_description/models/models.py
+++ extended_description/models/models.py
@@ -17,14 +17,15 @@ class SaleOrderLine(models.Model):
 	_inherit = 'sale.order.line'
 
 	@api.model
-	def create(self, vals):
-		sale_order_line = super(SaleOrderLine, self).create(vals)
-		if sale_order_line.product_id:
-			product_template = sale_order_line.product_id.product_tmpl_id
-			if product_template.extended_description:
-				self.env['sale.order.line'].create({
-					'order_id': sale_order_line.order_id.id,
-					'display_type': 'line_note',
-					'name': product_template.extended_description,
-				})
-		return sale_order_line
+	def create(self, vals_list):
+		sale_order_lines = super(SaleOrderLine, self).create(vals_list)
+		for sale_order_line in sale_order_lines:
+			if sale_order_line.product_id:
+				product_template = sale_order_line.product_id.product_tmpl_id
+				if product_template.extended_description:
+					self.env['sale.order.line'].create({
+						'order_id': sale_order_line.order_id.id,
+						'display_type': 'line_note',
+						'name': product_template.extended_description,
+					})
+		return sale_order_lines
