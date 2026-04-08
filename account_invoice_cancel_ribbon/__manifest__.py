76020cfe9a267741246014c441b179b1d6727da4
Pablo Castelo <pablocv@gmx.es>
2026-04-08 14:04:27 +0200

    add anulada ribbon

---
+++ account_invoice_cancel_ribbon/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Cancel Ribbon",
+    "version": "16.0.1.0.0",
+    "summary": "Show red ANULADA ribbon on canceled invoices",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "zvERP.com",
+    "website": "https://zverp.com",
+    "depends": ["account"],
+    "data": ["views/account_move_views.xml"],
+    "installable": True,
+}
